86bf35620636f223b051f29eb8e15be69f8e5897
Sky_Dog <362664609@qq.com>
2023-07-11 00:30:05 +0800

    feat: add some docs

--- src/main/java/com/west2/entity/Template.java
+++ src/main/java/com/west2/entity/Template.java
@@ -8,13 +8,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Accessors(chain = true)
-@TableName("`test`")
+/**
+ * 测试实体类
+ * @author 天狗
+ * @date 2023-07-10
+ */
+@Data                       // 生成getter、setter方法
+@AllArgsConstructor         // 全参构造器
+@NoArgsConstructor          // 无参构造器
+@Accessors(chain = true)    // 启用链式调用
+@TableName("`test`")        // 指定表名
 public class Template extends DataEntity<Template> {
 
-    String balabala;
+    /**
+     * 测试字段......
+     */
+    String name;
+    String age;
 
 }
--- src/main/java/com/west2/mapper/TemplateMapper.java
+++ src/main/java/com/west2/mapper/TemplateMapper.java
@@ -4,6 +4,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.west2.entity.Template;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * template表的mapper接口
+ * @author 天狗
+ * @date 2023/7/11
+ */
 @Mapper
 public interface TemplateMapper extends BaseMapper<Template> {
 
