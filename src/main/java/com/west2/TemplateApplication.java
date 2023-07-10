fce869ec7d49f13c601e5588d4044a34324d0240
Sky_Dog <362664609@qq.com>
2023-07-11 00:34:07 +0800

    feat: add some docs

--- src/main/java/com/west2/TemplateApplication.java
+++ src/main/java/com/west2/TemplateApplication.java
@@ -3,6 +3,11 @@ package com.west2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主启动类
+ * @author 天狗
+ * @date 2023/07/11
+ */
 @SpringBootApplication
 public class TemplateApplication {
 
--- src/main/java/com/west2/service/TemplateService.java
+++ src/main/java/com/west2/service/TemplateService.java
@@ -4,6 +4,11 @@ import com.west2.entity.Template;
 import com.west2.mapper.TemplateMapper;
 import org.springframework.stereotype.Service;
 
+/**
+ * 测试用的template表Service层
+ * @author 天狗
+ * @date 2023/07/11
+ */
 @Service
 public class TemplateService extends CrudService<TemplateMapper, Template>{
 
